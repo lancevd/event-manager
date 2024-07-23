f75a28f26ecf226b8c859d6dd8b23a12fbef7d5d
lancevd <olamosob@gmail.com>
2024-07-23 20:05:00 +0100

    Added heAader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header/>
       <h1>Hello World</h1>
     </div>
   );
