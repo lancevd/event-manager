bcb85a0dea245172e7d745075709b48ab2ac393e
lancevd <olamosob@gmail.com>
2024-07-24 05:30:36 +0100

    Events grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
+import Events from '../components/home/Events'
 
 const Home = () => {
   return (
     <main>
         <Hero/>
+        <Events/>
     </main>
   )
 }
