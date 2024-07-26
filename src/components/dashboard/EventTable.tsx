15d28048e79aec9238c7b1c82d13013977f11e57
lancevd <olamosob@gmail.com>
2024-07-26 10:03:28 +0100

    refresh when add event

--- src/components/dashboard/EventTable.tsx
+++ src/components/dashboard/EventTable.tsx
@@ -4,7 +4,7 @@ import ViewModal from "./modals/ViewModal";
 import EditModal from "./modals/EditModal";
 import DeleteModal from "./modals/DeleteModal";
 import axios from "axios";
-
+import Spinner from "../Spinner";
 
 interface Event {
   _id: string;
@@ -21,50 +21,30 @@ const EventTable: React.FC = () => {
   const [showViewModal, setShowViewModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState<any>(null);
+  const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
   const [events, setEvents] = useState<Event[] | null>(null);
-  const [loading, setLoading] = useState<boolean | null>(false)
+  const [loading, setLoading] = useState<boolean>(false);
+  const [refresh, setRefresh] = useState<boolean>(false); // New state to trigger refresh
 
   const handleCreateClick = () => setShowCreateModal(true);
-  const handleViewClick = (event: any) => {
+  const handleViewClick = (event: Event) => {
     setSelectedEvent(event);
     setShowViewModal(true);
   };
-  const handleEditClick = (event: any) => {
+  const handleEditClick = (event: Event) => {
     setSelectedEvent(event);
     setShowEditModal(true);
   };
-  const handleDeleteClick = (event: any) => {
+  const handleDeleteClick = (event: Event) => {
     setSelectedEvent(event);
     setShowDeleteModal(true);
   };
-
-  // const events = [
-  //   {
-  //     id: 1,
-  //     name: "Event 1",
-  //     category: "Conference",
-  //     location: "New York",
-  //     description: "This is event 1",
-  //     date: "2024-08-01",
-  //     time: "10:00 AM",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Event 2",
-  //     category: "Workshop",
-  //     location: "San Francisco",
-  //     description: "This is event 2",
-  //     date: "2024-09-15",
-  //     time: "2:00 PM",
-  //   },
-  // ];
 
   const token = localStorage.getItem("token");
 
   useEffect(() => {
     const fetchEvents = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
         const response = await axios.get(
           `${process.env.REACT_APP_BASE_URL}api/events/myevents`,
@@ -75,25 +55,30 @@ const EventTable: React.FC = () => {
           }
         );
 
-        if (response.status ===200) {
+        if (response.status === 200) {
           setEvents(response.data);
           console.log(response);
-          setLoading(false)
         }
       } catch (error) {
         console.error("Error fetching events:", error);
+      } finally {
         setLoading(false);
       }
     };
 
     fetchEvents();
-  }, []);
+  }, [token, refresh]); // Dependency on token and refresh state
+
+  const handleEventCreated = () => {
+    setShowCreateModal(false);
+    setRefresh(!refresh); // Toggle refresh state to trigger useEffect
+  };
 
   return (
-    <div className="contain ">
+    <div className="contain">
       <button
         onClick={handleCreateClick}
-        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm py-2 px-3 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white border "
+        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm py-2 px-3 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white border"
       >
         Add Event
       </button>
@@ -110,41 +95,48 @@ const EventTable: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {!events ? "Loading..." : events.map((event, index) => (
-              <tr key={index} className="border-b border-gray-600 py-1">
-                <td className="py-2 pl-3">{event.name}</td>
-                <td className="p-2">{event.location}</td>
-                <td className="p-2">{event.description.slice(0,20)}</td>
-                <td className="p-2">{event.date}</td>
-                <td className="p-2">{event.time}</td>
-                <td className="p-2 flex flex-col lg:flex-row gap-1">
-                  <button
-                    onClick={() => handleViewClick(event)}
-                    className="bg-gray-500 text-white px-2 py-1 rounded"
-                  >
-                    View
-                  </button>
-                  <button
-                    onClick={() => handleEditClick(event)}
-                    className="bg-yellow-500 text-white px-2 py-1 rounded ml-2"
-                  >
-                    Edit
-                  </button>
-                  <button
-                    onClick={() => handleDeleteClick(event)}
-                    className="bg-red-500 text-white px-2 py-1 rounded ml-2"
-                  >
-                    Delete
-                  </button>
+            {loading ? (
+              <tr className="w-full justify-center">
+                <td colSpan={6} className="text-center p-4">
+                  <Spinner />
                 </td>
               </tr>
-            ))}
+            ) : (
+              events &&
+              events.map((event, index) => (
+                <tr key={index} className="border-b border-gray-600 py-1">
+                  <td className="py-2 pl-3">{event.name}</td>
+                  <td className="p-2">{event.location}</td>
+                  <td className="p-2">{event.description.slice(0, 20)}</td>
+                  <td className="p-2">{event.date}</td>
+                  <td className="p-2">{event.time}</td>
+                  <td className="p-2 flex flex-col lg:flex-row gap-1">
+                    <button
+                      onClick={() => handleViewClick(event)}
+                      className="bg-gray-500 text-white px-2 py-1 rounded"
+                    >
+                      View
+                    </button>
+                    <button
+                      onClick={() => handleEditClick(event)}
+                      className="bg-yellow-500 text-white px-2 py-1 rounded ml-2"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDeleteClick(event)}
+                      className="bg-red-500 text-white px-2 py-1 rounded ml-2"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
-      {showCreateModal && (
-        <CreateModal onClose={() => setShowCreateModal(false)} />
-      )}
+      {showCreateModal && <CreateModal onClose={handleEventCreated} />}
       {showViewModal && selectedEvent && (
         <ViewModal
           event={selectedEvent}
