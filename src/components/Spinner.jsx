130b95b6735c21aa93f10b1d00c57a0099b299d2
lancevd <olamosob@gmail.com>
2024-07-26 09:47:51 +0100

    spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-xl h-6 w-6 border-t-4 border-b-4 border-gray-500"></div>
+    </div>
+  );
+};
+
+export default Spinner;
